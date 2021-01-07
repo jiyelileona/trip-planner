cf42dcf961aeec418f86abcef411c2f2788c8069
Jiye Li <15832778446@163.com>
2021-01-07 12:35:16 -0600

    create origin and destination list

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import regeneratorRuntime from 'regenerator-runtime';
 const originInputHandler = e => {
   if (e.keyCode == 13) {
     e.preventDefault();
+    originList.innerHTML = '';
     getResultsOfOrigin(e.target.value);
   }
 };
@@ -11,6 +12,7 @@ const originInputHandler = e => {
 const destinationInputHandler = e => {
   if (e.keyCode == 13) {
     e.preventDefault();
+    destinationList.innerHTML = '';
     getResultsOfDestination(e.target.value);
   }
 };
@@ -20,7 +22,7 @@ const getResultsOfOrigin = async origin => {
   const {features} = await res.json();
   features.map(feature => {
     const {place_name, text, center} = feature;
-    console.log(place_name, text, center);
+    showOriginList(text, place_name.split(',')[1], center[0], center[1]);
   });
 };
 
@@ -29,12 +31,38 @@ const getResultsOfDestination = async destination => {
   const {features} = await res.json();
   features.map(feature => {
     const {place_name, text, center} = feature;
-    console.log(place_name, text, center);
+    showDestinationList(text, place_name.split(',')[1], center[0], center[1]);
   });
+};
+
+const showOriginList = (name, address, lon, lat) => {
+  originList.insertAdjacentHTML(
+    'beforeend',
+    `
+    <li data-long="${lon}" data-lat="${lat}">
+      <div class="name">${name}</div>
+      <div>${address}</div>
+    </li>
+  `
+  );
+};
+
+const showDestinationList = (name, address, lon, lat) => {
+  destinationList.insertAdjacentHTML(
+    'beforeend',
+    `
+    <li data-long="${lon}" data-lat="${lat}">
+      <div class="name">${name}</div>
+      <div>${address}</div>
+    </li>
+  `
+  );
 };
 
 const originInput = document.querySelector('.origin-form input');
 const destinationInput = document.querySelector('.destination-form input');
+const originList = document.querySelector('.origins');
+const destinationList = document.querySelector('.destinations');
 
 originInput.addEventListener('keypress', e => originInputHandler(e));
 destinationInput.addEventListener('keypress', e => destinationInputHandler(e));
